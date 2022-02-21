276b4391a2902b320946c1fe94e56536c26f7494
Faizan <faizanqureshiism@gmail.com>
2022-02-21 19:31:40 +0530

    Starting with feeds

---
+++ components/Stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import faker from 'faker';
+
+
+function Stories() {
+  return (
+    <div>
+  {/* For stories we'll take up some fake people from faker.js*/}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+    </div>
+  )
+}
+
+export default Stories
