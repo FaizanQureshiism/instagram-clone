cdd565c740e1c7487e220403541570e510304834
Faizan <faizanqureshiism@gmail.com>
2022-02-21 23:38:04 +0530

    stories

--- components/Stories.js
+++ components/Stories.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import faker from 'faker';
-
+import React, { useEffect } from 'react';
+import faker from "faker";
 
 function Stories() {
+//useEffect allows you to refire this function by taking the update 
+//from the data and loads everytime the browser is refreshed or new data is added
+    useEffect( () => {
+        const suggestions = [...Array(20)].map((_,i) => ({
+          ...faker.helpers.contextualCard(),
+          id:i,
+        }));
+        
+        console.log(suggestions);
+    }, []);
+
   return (
     <div>
   {/* For stories we'll take up some fake people from faker.js*/}
