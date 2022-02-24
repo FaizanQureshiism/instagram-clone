fcf05934a06fd742076f93a40ff665e1acb44759
Faizan <faizanqureshiism@gmail.com>
2022-02-24 21:42:57 +0530

    Front-end done

--- components/Feed.js
+++ components/Feed.js
@@ -1,24 +1,31 @@
 import React from "react";
+import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
+import Suggestions from "./Suggestions";
 //2.00.29 
 function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
       {/* What we going for the feed is we'll have 3 grid on a large screen and when it is mobile form it will colapse into 1*/}
       <section className="col-span-2">
+
         {/* Stories */}
         <Stories />
 
         {/* Post */}
         <Posts />
-
-
       </section>
 
-      <section>
-        {/* Mini profile */}
+      <section className="hidden xl:inline-grid md:col-span-1">
+      <div className="fixed ">
+      {/* Mini profile */}
+        <MiniProfile />
+
         {/* Suggestions */}
+        <Suggestions />
+      </div>
+        
       </section>
     </main>
   );
---
+++ components/MiniProfile.js
@@ -0,0 +1,20 @@
+import React from 'react'
+//2.3.05
+function MiniProfile() {
+  return (
+    <div className='flex items-center justify-between mt-14 ml-10'>
+      <img className="w-16 h-16 rounded-full border p-[2px]"
+      src="https://links.papareact.com/3ke" alt=""
+      />
+
+      <div className='flex-1 mx-4'>
+        <h2 className="font-bold">faizanqureshi</h2>
+        <h3>Welcome to Instagram</h3>
+      </div>  
+
+      <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+    </div>
+  )
+}
+
+export default MiniProfile
