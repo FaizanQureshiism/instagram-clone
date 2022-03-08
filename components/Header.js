906054dbe84c167b40afd679523db0652d194f8b
Faizan <faizanqureshiism@gmail.com>
2022-03-08 18:37:07 +0530

    session

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,15 @@ import {
 } from "@heroicons/react/outline";
 
 import { HomeIcon } from "@heroicons/react/solid";
-//57.01
+import { useSession } from "next-auth/react";
+
 function Header() {
+  const { data: session } = useSession();
+  console.log(session);
+  //so this session bring out the info of the user that has login displaying gmail address from google authentication
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
-      
         {/* Left */}
         <div className="relative hidden lg:inline-grid w-24">
           <Image
@@ -54,28 +57,25 @@ function Header() {
 
           <div className="relative navBtn">
             <PaperAirplaneIcon className="navBtn rotate-45" />
-            <div className="absolute -top-1 -right-2 text text-xs w-5 h-5 bg-red-500 
-            rounded-full flex items-center justify-center animate-pulse text-white">
-            3</div>
-
+            <div
+              className="absolute -top-1 -right-2 text text-xs w-5 h-5 bg-red-500 
+            rounded-full flex items-center justify-center animate-pulse text-white"
+            >
+              3
+            </div>
           </div>
-          
-  
+
           <PlusCircleIcon className="navBtn" />
           <UserGroupIcon className="navBtn" />
           <HeartIcon className="navBtn" />
 
-        <img 
-        src="https://links.papareact.com/3ke"
-        alt="profile pic"
-        className="h-10 rounded-full cursor-pointer" />
-          
-          
-
+          <img
+            //src="https://links.papareact.com/3ke"
+            src={session?.user?.image}
+            alt="profile pic"
+            className="h-10 rounded-full cursor-pointer"
+          />
         </div>
-        
-
-
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+//Now we'll wrap our entire app in sessions for login and logout
+import { SessionProvider } from "next-auth/react"
+//3.11
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />; 
+    </SessionProvider>
+  );
+  
 }
 
 export default MyApp
